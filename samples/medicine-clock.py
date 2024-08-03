ee264e0da4e49d534d3d17e67c3d2d18d72c1af2
yamagame <yamagame@mac.com>
2024-08-03 10:26:42 +0900

    次の日の5時に変更

--- samples/medicine-clock.py
+++ samples/medicine-clock.py
@@ -1,24 +1,27 @@
 from machine import I2C, PWM, Pin, Timer
 from urtc import DS1307
+import re
+import time
+import datetime
 import ssd1306
 import utime
-import time
 
 WIDTH = 128
 HEIGHT = 64
 
-RED_LED = 10
-BLUE_LED = 11
+RED_LED = 7  # 10
+BLUE_LED = 8  # 11
 PWM_PIN = 20
 SW_PIN = 21
 LED_PIN = 'LED'
+MOD_PIN = 16
 
 active = True
-next_action_seconds = 10
 
 blue_led = Pin(BLUE_LED, Pin.OUT)
 red_led = Pin(RED_LED, Pin.OUT)
 sw = Pin(SW_PIN, Pin.IN, Pin.PULL_UP)
+mod = Pin(MOD_PIN, Pin.IN, Pin.PULL_UP)
 led = Pin(LED_PIN, Pin.OUT)
 led.value(0)
 
@@ -58,7 +61,7 @@ C6 = 1046.502
 
 mspb = 156
 
-melody = [D5, E5, 0, D5, E5, 0, C6, B5, 0, 0, G5]
+melody = [D5, E5, 0, D5, E5, 0, D5, E5, 0, 0, G5]
 beat_i = 0
 
 
@@ -85,11 +88,48 @@ def beat(timer):
 timestamp = ""
 
 
+def parsetime(timestamp):
+    r = re.match('(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)', timestamp)
+    if r:
+        year = int(r.group(1))
+        month = int(r.group(2))
+        day = int(r.group(3))
+        hour = int(r.group(4))
+        minute = int(r.group(5))
+        second = int(r.group(6))
+        return datetime.datetime(
+            year=year, month=month, day=day,
+            hour=hour, minute=minute, second=second
+        )
+
+
+def addtime(dt1, deltasec):
+    if dt1:
+        dt2 = datetime.timedelta(seconds=deltasec)
+        dt3 = dt1 + dt2
+        return "{0:04}{1:02}{2:02}{3:02}{4:02}{5:02}".format(
+            dt3.year, dt3.month, dt3.day,
+            dt3.hour, dt3.minute, dt3.second
+        )
+    return "0"
+
+
+def nexttime(pushedtime):
+    is_debug = mod.value() == 0
+    if is_debug:
+        # デバッグモードは10秒でタイムアウト
+        return addtime(parsetime(pushedtime), 10)
+    # 次の日の朝5時まで
+    t = parsetime(pushedtime)
+    t = datetime.datetime(year=t.year, month=t.month, day=t.day)
+    return addtime(t, (24+5)*60*60)
+
+
 def checkbutton(timestamp):
     global beat_i
     global active
     pushedtime = read_eeprom(14)
-    if int(pushedtime)+next_action_seconds < int(timestamp):
+    if int(nexttime(pushedtime)) < int(timestamp):
         active = False
     if not active and sw.value() == 0:
         active = True
@@ -124,4 +164,4 @@ while True:
     checkbutton(timestamp)
     utime.sleep(0.5)
     pushedtime = checkbutton(timestamp)
-    print(timestamp, pushedtime)
+    print(timestamp, pushedtime, nexttime(pushedtime))
